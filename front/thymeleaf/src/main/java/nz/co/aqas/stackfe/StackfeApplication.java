5969f915b38536c7d4def0f70398a275e87b9eab
tony harold <tony.harold2@datacom.co.nz>
2022-09-22 11:10:55 +1200

    Add in an unused, incomplete, unconfigured thymeleaf front end

---
+++ front/thymeleaf/src/main/java/nz/co/aqas/stackfe/StackfeApplication.java
@@ -0,0 +1,13 @@
+package nz.co.aqas.stackfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackfeApplication.class, args);
+	}
+
+}
---
+++ front/thymeleaf/src/test/java/nz/co/aqas/stackfe/StackfeApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.aqas.stackfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
